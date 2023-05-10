03bdd0caed45c04f97becd566e7aabca46ed2aa0
tezarsurya <tzr.surya@gmail.com>
2023-05-10 10:54:12 +0700

    add parseTIme param to db conn string

--- database/database.go
+++ database/database.go
@@ -6,7 +6,7 @@ import (
 )
 
 func ConnectDB() (*gorm.DB, error) {
-	dsn := "phpmyadmin:tzrsurya212@tcp(127.0.0.1:3306)/hehe-marketplace"
+	dsn := "phpmyadmin:tzrsurya212@tcp(127.0.0.1:3306)/hehe-marketplace?charset=utf8mb4&parseTime=true"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	return db, err
 }
