c745fb1efcc3624747139c11867679306ebebd1a
tezarsurya <tzr.surya@gmail.com>
2023-05-13 22:48:49 +0700

    add env variable to connection string

--- database/database.go
+++ database/database.go
@@ -1,12 +1,19 @@
 package database
 
 import (
+	"fmt"
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func ConnectDB() (*gorm.DB, error) {
-	dsn := "phpmyadmin:tzrsurya212@tcp(127.0.0.1:3306)/hehe-marketplace?charset=utf8mb4&parseTime=true"
+	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?charset=utf8mb4&parseTime=true",
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	return db, err
 }
